47a4dd43271dbfe32edcf8f99d6381b5cb09a04d
Marcos junior oliveira <oliveiraregojunior@gmail.com>
2024-09-11 23:25:44 -0300

    Criaçao Banco de dados 56:48

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,1 +1,28 @@
 // arquivo para popular banco de dados com dados ficticios
+
+import dayjs from "dayjs";
+import { client, db } from ".";
+import { goalCompletions, goals } from "./schema";
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db.insert(goals).values([
+    {title: 'Acordar cedo' , desiredWeeklyFrequency: 5},
+    {title: 'Me exercitar' , desiredWeeklyFrequency: 3},
+    {title: 'Meditar' , desiredWeeklyFrequency: 1},
+  ]).returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+
+  await db.insert(goalCompletions).values([
+    {goalId: result[0].id, createAt : startOfWeek.toDate()},
+    {goalId: result[0].id, createAt : startOfWeek.add(1, 'day').toDate()},
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
